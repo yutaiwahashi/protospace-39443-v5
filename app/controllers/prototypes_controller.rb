2b2c1c78f6d24c7e9e5f02af6587855201d741e4
yutaiwahashi <yuta.iwahahsi@gmail.com>
2023-12-16 15:32:29 +0900

    投稿機能

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,7 +1,8 @@
 class PrototypesController < ApplicationController
-  before_action :set_prototype, except: [:index, :new]
+  before_action :set_prototype, except: [:index, :new, :create]
 
   def index
+    @prototype = Prototype.include(:user)
   end
 
   def new
@@ -9,6 +10,17 @@ class PrototypesController < ApplicationController
   end
 
   def create
+    @prototype = Prototype.new(prototype_params)
+    if @prototype.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
+  private
+
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
 end
